4fbc5425be7e96c17dcc0f2a6e79f9df46523605
mehdis <sahtali.mehdi@gmail.com>
2021-05-04 17:16:36 +0200

    grand nettoyage de printemps - avec suppressions

--- expo/js/recadrage.js
+++
@@ -1,9 +0,0 @@
-var canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth;
-canvas.height = 3/5*canvas.width; 
-function resize(){
-	"use strict";
-	canvas.width = window.innerWidth;
-	canvas.height = 3/5*canvas.width; 
-}
-window.onresize=resize;

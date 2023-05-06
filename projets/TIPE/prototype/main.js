a65f25a5be93ce1d49ae9832e1c7a2a0ba404d53
Mehdi S <sahtali.mehdi@gmail.com>
2023-05-06 09:56:34 +0200

    séparation pour lisibilité

---
+++ projets/TIPE/prototype/main.js
@@ -0,0 +1,46 @@
+const paX = document.getElementById("accX");
+            const paY = document.getElementById("accY");
+            const paZ = document.getElementById("accZ");
+
+            const poZ = document.getElementById("oriZ");
+
+            /*const sensor = new AbsoluteOrientationSensor();
+
+            Promise.all([
+                navigator.permissions.query({ name: "accelerometer" }),
+                navigator.permissions.query({ name: "magnetometer" }),
+                navigator.permissions.query({ name: "gyroscope" }),
+            ]).then((results) => {
+                if (results.every((result) => result.state === "granted")) {
+                    sensor.start();
+                    // …
+
+                } else {
+                    console.log("No permissions to use AbsoluteOrientationSensor.");
+                }
+            });
+            */
+            window.addEventListener("deviceorientation", (event) => {
+                poZ.innerHTML=`${event.alpha.toFixed(2)} : ${event.beta.toFixed(2)} : ${event.gamma.toFixed(2)}`;
+            });
+            
+            let aX=0;
+            let aY=0;
+            let aZ=0;
+            console.log("works");
+            const acl = new Accelerometer({ frequency: 60 });
+
+            acl.addEventListener("reading", () => 
+            {
+            aX=acl.x.toFixed(5);
+            aY=acl.y.toFixed(5);
+            aZ=acl.z.toFixed(5);
+            paX.innerHTML=`Accélération selon l'axe X: ${aX}`;
+            paY.innerHTML=`Accélération selon l'axe Y:${aY}`;
+            paZ.innerHTML=`Accélération selon l'axe Z:${aZ}`;
+                
+            if(aX>3){}
+            });
+
+            acl.start();
+            
